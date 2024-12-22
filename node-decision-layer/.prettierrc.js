dbf672f998eed3b023ddf182ad170fbb4a15f42b
mikkan-39 <egor.altsoft2@gmail.com>
2024-12-22 15:31:17 +0300

    init

---
+++ node-decision-layer/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  endOfLine: "auto",
+  // Discuss needed (suggested in RN 0.64.2)
+  // arrowParens: 'avoid',
+}
