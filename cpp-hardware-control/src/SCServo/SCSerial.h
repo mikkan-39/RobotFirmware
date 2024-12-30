e7c8fc12b9f97cb4f8de29f396dcb9ab0716f0f3
Егор <egor.elkin@sbermarket.ru>
2024-12-30 16:40:13 +0300

    SCServo init

---
+++ cpp-hardware-control/src/SCServo/SCSerial.h
@@ -0,0 +1,33 @@
+﻿/*
+ * SCSerial.h
+ * hardware interface layer for waveshare serial bus servo
+ */
+
+#ifndef _SCSERIAL_H
+#define _SCSERIAL_H
+
+#include "Arduino.h"
+#include "SCS.h"
+
+class SCSerial : public SCS {
+ public:
+  SCSerial();
+  SCSerial(u8 End);
+  SCSerial(u8 End, u8 Level);
+
+ protected:
+  virtual int writeSCS(unsigned char *nDat, int nLen);  // output nLen byte
+  virtual int readSCS(unsigned char *nDat, int nLen);   // input nLen byte
+  virtual int writeSCS(unsigned char bDat);             // output 1 byte
+  virtual void rFlushSCS();                             //
+  virtual void wFlushSCS();                             //
+ public:
+  unsigned long int IOTimeOut;  // I/O timeout
+  HardwareSerial *pSerial;      // serial pointer
+  int Err;
+
+ public:
+  virtual int getErr() { return Err; }
+};
+
+#endif
---
+++ cpp-hardware-control/src/SCServo/SCServo.h
@@ -0,0 +1,12 @@
+/*
+ * SCServo.h
+ * interface for waveshare serial bus servo
+ */
+
+#ifndef _SCSERVO_H
+#define _SCSERVO_H
+
+#include "SCSCL.h"
+#include "SMS_STS.h"
+
+#endif
